4bb6b1f9d62b807a2b2df96790afe59533bb2584
abelgarcia2 <abelgg2000@gmail.com>
2021-11-12 13:55:17 +0100

    Se corrige el script de creacion de la BD para poder guardar hora junto con las fechas

--- script-bd/crearBd.sql
+++ script-bd/crearBd.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Vehiculos(
     id INT PRIMARY KEY,
     matricula VARCHAR(20) NOT NULL,
-    fechaEntrada DATE NOT NULL,
-    fechaSalida DATE,
+    fechaEntrada TIMESTAMP NOT NULL,
+    fechaSalida TIMESTAMP,
     pagado BOOLEAN
 );
